129313e3586f570e127d0e795fd31c92cb24190a
Samuel Beek <ik@samuelbeek.com>
2016-04-30 20:09:09 +0200

    add post call

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('Hello world');
+});
+
+// returns name property
+app.post('/', function (req, res) {
+  res.send(req.body.name);
 });
 
 app.listen(3000, function () {
