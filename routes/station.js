2dbae7384aba87fccedd8df0a647a647d156d777
Samuel Beek <ik@samuelbeek.com>
2016-07-15 16:12:41 +0200

    sync on big scale with url checking (not working)

--- routes/station.js
+++ routes/station.js
@@ -4,9 +4,9 @@ var Podcast = require('../models/podcast');
 var _ = require('lodash');
 var middlewares = require("../utils/middlewares");
 var podcastSearch = require('../utils/podcastSearch.js');
+var sync = require('../utils/sync.js')
 var urlExists = require('url-exists');
 var settle = require('promise-settle');
-var rp = require('request-promise');
 
 module.exports = function(app){
 
@@ -15,7 +15,6 @@ module.exports = function(app){
       res.send(results);
     });
   });
-
 
   app.get('/station/:stationId/', middlewares.stationByIdWithPodcasts ,function(req, res){
     res.send(req.station);
@@ -64,129 +63,49 @@ app.post('/station/:stationId/show', middlewares.stationById, function(req, res)
         })
       }
     });
+  });
 
+  app.post('/station/:stationId/sync', function(req, res){
+    sync().syncStation(req.params.stationId, function(result, error){
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(result);
+      }
+    });
   });
 
   app.post('/station/sync', function(req, res){
 
-    var promises = []
+    Station.find().then(function(results, err) {
 
-
-    Station.find(function (err, stations) {
       if (err) {
         console.error(err);
         res.send({"error":err})
-      }
+      } else {
+        var promises = []
+        _.each(results, function(station){
 
-      _.each(stations, function(station){
-        promises.push(
-          new Promise(function(resolve, reject){
-
-          })
-        )
-      })
-
-
-    })
-
-
-    // if all promises succeeded, send them to thte client
-    Promise.all(promises).then(function(resolvedPromises) {
-      res.send(resolvedPromises);
-    });
-
-
-  });
-
-  app.post('/station/:stationId/sync', middlewares.stationByIdWithPodcasts, function(req, res){
-      var station = req.station;
-      var promises = []
-
-      _(station.shows).forEach(function(show){
-
-        // get all shows
           promises.push(
-            new Promise(function (resolve, reject) {
-              podcastSearch().getEpisodesByShowId(show.audioSearchId).then(function(results, error){
-                var episodePromises = []
-
-                _(results).forEach(function(unparsedPodcast) {
-                  episodePromises.push(
-
-                    new Promise(function(resolve, reject){
-
-                      var podcastUrl = unparsedPodcast.audio_files[0].url[0];
-
-                      // Only create podcast objects for urls that are actually legit.
-                      urlExists(podcastUrl, function(err, exists) {
-                        if (exists == true) {
-                          // create new podcast
-                          var newPodcast = {
-                            title: unparsedPodcast.title,
-                            _creator: show._id,
-                            description: unparsedPodcast.description,
-                            audioUrl: podcastUrl,
-                            audioSearchId: unparsedPodcast.id,
-                            imageUrl: show.imageUrl,
-                            shareUrl: unparsedPodcast.digital_location,
-                            dateCreated: unparsedPodcast.date_added,
-                            duration: unparsedPodcast.duration
-                          }
-
-                          // add podcast if it isn't around, if not: update
-                          Podcast.findOneAndUpdate({audioSearchId: unparsedPodcast.id}, newPodcast, {upsert: true}, function (err, result) {
-                            if (err) {
-                              reject(err);
-                              return console.error(err);
-                            }
-                            resolve(result);
-                          });
-
-                        } else {
-                          reject("url wasn't reachable");
-                        }
-                      });
-
-
-
-
-                    })
-                  )
-                });
-
-                settle(episodePromises).then(function(results){
-                  var resolvedPromises = []
-
-                  _.each(results, function(result) {
-
-                    if (result.isFulfilled()) {
-                      resolvedPromises.push(result.value());
-                    } else {
-                      console.log('Promise is rejected', result.reason());
-                    }
-
-                  })
-
-                  console.log("these are fulfilled", resolvedPromises);
-
-                  var podcasts = show.podcasts.concat(resolvedPromises);
-                  podcasts = _.uniqBy(podcasts, 'audioSearchId');
-                  show.podcasts = podcasts;
-                  show.save(function(err, result){
-                      if (err) return console.error(err);
-                      resolve(result.toJSON());
-                  })
-                })
-              })
+            new Promise(function(resolve, reject){
+              sync().syncStation(station._id, function(result, error){
+                console.log("syncing station", station.title , station._id);
+                if (error) {
+                  res.send(error);
+                } else {
+                  res.send(result);
+                }
+              });
             })
-          );
-      })
+          )
+        })
 
-      // if all promises succeeded, send them to thte client
-      Promise.all(promises).then(function(resolvedPromises) {
-        res.send(resolvedPromises);
-      });
+        // if all promises succeeded, send them to thte client
+        Promise.all(promises).then(function(resolvedPromises) {
+          res.send(resolvedPromises);
+        });
 
+      }
   });
-
+})
 }
